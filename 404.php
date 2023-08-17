8e36b2daaf821b89df27c13ba93229285b9c0f29
Valeriy <141771074+hokken111@users.noreply.github.com>
2023-08-17 19:51:15 +0300

    #233

---
+++ 404.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>CV LEBRET Damien</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+        <meta name="description" content="CV LEBRET Damien Developpeur WEB PORTFOLIO ">
+        <meta name="Keywords" content="PORTFOLIO CV LEBRET Damien Developpeur WEB">
+        <meta name="Content-Language" content="fr">
+        <meta name="Author" content="LEBRET Damien">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" type="text/css" href="css/screen.css">      
+        <link rel="shortcut icon" href="favicon.ico" >
+		
+		
+		<!--[if lt IE 9]> // version inférieur a IE 9
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<![endif]-->
+		
+</head>
+<body>
+	
+	<div id="wrapper">
+	<div id="qcq">
+		Erreur 404<br>
+		Page introuvable
+	</div>
+
+	<div class="brique">
+		<div class="content_wp">
+			<table class="pc50">
+				<tr>
+					<td class="bg_gf"><a class="content" id='menu_cv' href="/cv">MON CV</a></td>
+				</tr>
+				<tr>
+					<td class="bg_gc"><a class="content" id='menu_portfolio' href="/mon_portfolio">PORTFOLIO</a></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	</div>
+	
+</body>    
+</html>
